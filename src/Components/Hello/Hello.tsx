b68ffaf2102ed1a97d84a077c02bcba06bf5e0ee
Jeeva-Sudhegan <jeevasudhegan@gmail.com>
2019-11-16 13:22:33 +0530

    configured prettier and eslint according to each other and added script to run both

--- src/Components/Hello/Hello.tsx
+++ src/Components/Hello/Hello.tsx
@@ -1,25 +1,50 @@
-import React, { Component, ReactNode, ReactElement, FunctionComponent, FC } from 'react';
+import React, {
+	Component,
+	ReactNode,
+	ReactElement,
+	FunctionComponent,
+	FC,
+} from 'react';
 
 export interface HelloProps {
-    compiler?: string;
-    framework?: string;
+	compiler?: string;
+	framework?: string;
 }
 
 const defaultHelloProps: HelloProps = {
-    compiler: "compiler",
-    framework: "framework"
-}
+	compiler: 'compiler',
+	framework: 'framework',
+};
 
 // SFC => Stateless Functional Component for setting default props
-export const HelloFunctionComponent : FC<HelloProps> = (props:HelloProps):ReactElement => <h1>Hello from {props.compiler} and {props.framework} from Functional Component</h1>;
+export const HelloFunctionComponent: FC<HelloProps> = (
+	props: HelloProps,
+): ReactElement => (
+	<h1>
+		Hello from {props.compiler} and{' '}
+		{props.framework} from
+		Functional Component
+	</h1>
+);
 
 HelloFunctionComponent.defaultProps = defaultHelloProps;
 
-export class HelloClassComponent extends Component<HelloProps, {}> {
-    static defaultProps: HelloProps = defaultHelloProps;
-    render(): ReactNode {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework} from Class Component</h1>
-    }
+export class HelloClassComponent extends Component<
+	HelloProps,
+	{}
+> {
+	static defaultProps: HelloProps = defaultHelloProps;
+	render(): ReactNode {
+		return (
+			<h1>
+				Hello from{' '}
+				{this.props.compiler}{' '}
+				and{' '}
+				{this.props.framework}{' '}
+				from Class Component
+			</h1>
+		);
+	}
 }
 
 // ReactNode => JSX element type
