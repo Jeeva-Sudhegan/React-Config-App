6e4cc4a845437880d026993341442f9564d97e4a
Jeeva-Sudhegan <jeevasudhegan@gmail.com>
2019-11-19 19:06:06 +0530

    added hot and open property from package script to dev webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,10 @@ const common = require("./webpack.common");
 const config = merge(common, {
 	devtool: "inline-source-map",
 	mode: "development",
+	devServer: {
+		open: true,
+		hot: true,
+	},
 });
 
 module.exports = config;
