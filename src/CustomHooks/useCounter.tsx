132c3b35e89d3241911eae7adae61fad708a4636
Jeeva-Sudhegan <jeevasudhegan@gmail.com>
2020-02-06 21:31:41 +0530

    removed eslint-disable comments

--- src/CustomHooks/useCounter.tsx
+++ src/CustomHooks/useCounter.tsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
 
-// eslint-disable-next-line import/prefer-default-export
 export const useCounter = initialCount => {
 	const [count, setCount] = useState(
 		initialCount,
 	);
 	const handleClick = (): void => {
 		setCount(
-			// eslint-disable-next-line no-shadow
 			(count: number) =>
 				count + 1,
 		);
