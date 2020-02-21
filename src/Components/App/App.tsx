8bc1a9761518f39a7592b722e56482ba422d43a9
Jeeva-Sudhegan <jeevasudhegan@gmail.com>
2020-02-22 00:55:49 +0530

    removed node_modules from .dockerignore file and added style to Loading fallback to suspense

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,17 +1,25 @@
 import React, {
-	ReactElement, lazy,
+	ReactElement, lazy, Suspense
 } from "react";
+import CSS from "csstype";
+
+const loadingStyle : CSS.Properties = {
+	fontWeight: "bold",
+	fontSize: "20px"
+}
 
 const HelloFunctionComponent = lazy( () => import("../Hello/Hello") );
-const Counter = lazy( () => import("../Counter/Counter"));
-const TimeCounter = lazy(() => import("../TimeCounter/TimeCounter"));
+const Counter = lazy( () => import("../Counter/Counter") );
+const TimeCounter = lazy(() => import("../TimeCounter/TimeCounter") );
 
 const App = (): ReactElement => (
 	<div className="App">
 		<h1> Hello React Application </h1>
-		<HelloFunctionComponent />
-		<Counter>Click Me!</Counter>
-		<TimeCounter />
+		<Suspense fallback = { <div style = { loadingStyle }>Loading...</div> }>
+			<HelloFunctionComponent />
+			<Counter>Click Me!</Counter>
+			<TimeCounter />
+		</Suspense>
 	</div>
 );
 
