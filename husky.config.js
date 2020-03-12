44211524bc4e29b14becc09dacc8cbf2a2776d7a
Jeeva-Sudhegan <jeevasudhegan@gmail.com>
2020-03-12 16:15:04 +0530

    parameters of multiple method is changed from array to spread

--- husky.config.js
+++ husky.config.js
@@ -1,8 +1,8 @@
-const multiple = arr => arr.join(" && ");
+const multiple = (...arr) => arr.join(" && ");
 
 module.exports = {
     "hooks": {
-        "pre-commit": multiple(["npm run format", "git add ."]),
+        "pre-commit": multiple("npm run format", "git add ."),
         "pre-push": "npm run test"
     }
 }
