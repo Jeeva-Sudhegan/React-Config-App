c924d017972bd88ceb3ed6d942363cb950fccf68
Jeeva-Sudhegan <jeevasudhegan@gmail.com>
2020-04-04 16:13:00 +0530

    made TimeCounter component mount and unmount on button click

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,5 +1,5 @@
 import React, {
-	ReactElement, lazy, Suspense
+	ReactElement, lazy, Suspense, useState
 } from "react";
 import CSS from "csstype";
 
@@ -12,15 +12,21 @@ const HelloFunctionComponent = lazy( () => import("../Hello/Hello") );
 const Counter = lazy( () => import("../Counter/Counter") );
 const TimeCounter = lazy(() => import("../TimeCounter/TimeCounter") );
 
-const App = (): ReactElement => (
+const App = (): ReactElement => {
+	const [ checked, setChecked ] = useState( false );
+	const handleCheck = () => {
+		setChecked( prevChecked => !prevChecked );
+	}
+	return (
 	<div className="App">
 		<h1> Hello React Application </h1>
 		<Suspense fallback = { <div style = { loadingStyle }>Loading...</div> }>
 			<HelloFunctionComponent />
 			<Counter>Click Me!</Counter>
-			<TimeCounter />
+			<button onClick = { handleCheck } >Display</button>
+			{ checked && <TimeCounter /> }
 		</Suspense>
 	</div>
-);
+)};
 
 export default App;
--- src/Components/TimeCounter/TimeCounter.tsx
+++ src/Components/TimeCounter/TimeCounter.tsx
@@ -2,10 +2,6 @@ import React, { useEffect, useState, ReactElement } from "react";
 
 const TimeCounter = (): ReactElement => {
     let interval;
-    const [ checked, setChecked ] = useState( false );
-	const handleCheck = () => {
-		setChecked( prevChecked => !prevChecked );
-	}
     const [ now, setNow ] = useState<Date>(new Date());
     useEffect(() => {
         interval = setInterval(() => {
@@ -13,12 +9,7 @@ const TimeCounter = (): ReactElement => {
         }, 1000);
         return () => clearInterval(interval);
     }, []);
-    return (
-        <>
-            <button onClick = { handleCheck } >Display</button>
-            { checked && <h1>{ now.toLocaleTimeString() }</h1> }
-        </>
-    )
+    return <h1>{ now.toLocaleTimeString() }</h1>
 }
 
 export default TimeCounter;
